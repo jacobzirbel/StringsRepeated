3a3f10e35aa8828fe1e68dd7f31e3432e4a7b9f8
Jacob Zirbel <jacobzirbel@gmail.com>
2020-05-05 01:55:29 -0500

    remove global variables

--- first-improved/fi-list.js
+++ first-improved/fi-list.js
@@ -1,79 +1,64 @@
-const [Node, LinkedList] = require("../LinkedList");
-module.exports = simpleList;
+const [Node, LinkedList, buildInputList] = require("../LinkedList");
+module.exports = fiList;
 
-const inputListHead = new Node("^");
-const keep = new LinkedList();
-const data = {};
-const finalData = {};
-
-function simpleList(input) {
-	buildInputList(input);
-
-	searchList();
-
-	let current = keep.first.next;
-	while (current) {
-		finalData[current.value] = data[current.value];
-		current = current.next;
+function fiList(input) {
+	const finalData = {};
+	let inputList = buildInputList(input);
+	let data = search(inputList);
+	for (let k in data) {
+		if (data[k] > 1) {
+			finalData[k] = data[k];
+		}
 	}
 	return finalData;
 }
 
-function searchList() {
-	searchListWord();
-	n = 1;
-	while (searchListPhrase(++n)) {}
-}
-
-function searchListWord() {
-	// Initial search of list, separate function so makePhrase(1) isn't called
-	let current = inputListHead.next;
-	while (current) {
-		let val = current.value + " ";
-		if (data[val]) {
-			if (data[val] === 1) keep.add(val);
-			data[val]++;
-		} else {
-			data[val] = 1;
-		}
-		current = current.next;
-	}
-}
-
-function searchListPhrase(n) {
-	let again = false;
-	let more = true;
-	let current = inputListHead.next;
-	while (more) {
-		let str = current.makePhrase(n);
+function search(inputList) {
+	let data = {};
+	let again = true;
+	let starts = new LinkedList();
+	inputList.forEach((e, node) => {
+		let str = e + " ";
 		if (str) {
 			if (data[str]) {
-				if (data[str] === 1) keep.add(str);
-				data[str]++;
-				again = true;
+				if (isNaN(data[str])) {
+					starts.add(data[str]);
+					starts.add(node);
+					data[str] = 2;
+				} else {
+					starts.add(node);
+					data[str]++;
+				}
 			} else {
-				data[str] = 1;
+				data[str] = node;
 			}
-			current = current.next;
-		} else {
-			more = false;
 		}
+	});
+
+	let n = 2;
+	while (again) {
+		again = false;
+		nextStarts = new LinkedList();
+		starts.forEach((node) => {
+			let str = node.makePhrase(n);
+			if (str) {
+				if (data[str]) {
+					if (isNaN(data[str])) {
+						nextStarts.add(data[str]);
+						nextStarts.add(node);
+						data[str] = 2;
+						again = true;
+					} else {
+						nextStarts.add(node);
+						data[str]++;
+					}
+				} else {
+					data[str] = node;
+				}
+			}
+		});
+		starts = nextStarts;
+		n++;
 	}
-
-	return again;
-}
-
-function buildInputList(input) {
-	let current = inputListHead;
-	let str = "";
-
-	for (let char of input) {
-		if (char === " ") {
-			current.next = new Node(str);
-			current = current.next;
-			str = "";
-		} else {
-			str += char;
-		}
-	}
+	return data;
 }
